a972ff560eef9d487971d781beb7c0329fd4f413
Jorge Sánchez Jorquera <Jsj_New04@hotmail.com>
2023-04-29 13:40:35 -0400

    Add project files.

---
+++ BluetoothTest/Pages/Index.razor.cs
@@ -0,0 +1,16 @@
+﻿using Blazor.Bluetooth;
+
+namespace BluetoothTest.Pages;
+
+public partial class Index
+{
+	private readonly IBluetoothNavigator BluetoothNavigator;
+
+    public Index(IBluetoothNavigator bluetoothNavigator)
+    {
+        BluetoothNavigator = bluetoothNavigator;
+    }
+
+
+
+}
