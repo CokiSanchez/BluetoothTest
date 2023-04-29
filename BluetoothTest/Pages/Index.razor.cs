d923d11f47669499e607fb25c0ec9323fc9846bb
Jorge Sánchez Jorquera <Jsj_New04@hotmail.com>
2023-04-29 14:26:34 -0400

    Buscar, conectar y desconectar

--- BluetoothTest/Pages/Index.razor.cs
+++ BluetoothTest/Pages/Index.razor.cs
@@ -1,16 +1,73 @@
 ﻿using Blazor.Bluetooth;
+using Microsoft.AspNetCore.Components;
 
 namespace BluetoothTest.Pages;
 
 public partial class Index
 {
-	private readonly IBluetoothNavigator BluetoothNavigator;
+    [Inject]
+    private IBluetoothNavigator BluetoothNavigator { get; set; } = null!;
+    private IDevice? Device { get; set; }
 
-    public Index(IBluetoothNavigator bluetoothNavigator)
+    private bool Connected { get; set; } = false;
+    private bool Available { get; set; } = false;
+    private string Error { get; set; } = string.Empty;
+
+    protected override async Task OnInitializedAsync()
     {
-        BluetoothNavigator = bluetoothNavigator;
+        BluetoothNavigator.OnAvailabilityChanged += async () => await OnAvailabilityChanged();
+        await BuscarServicio();
     }
 
+    private async Task OnAvailabilityChanged()
+    {
+        await BuscarServicio();
+    }
 
+    private async Task BuscarServicio()
+    {
+        try
+        {
+            Available = await BluetoothNavigator.GetAvailability();
+        }
+        catch (Exception)
+        {
+            throw;
+        }
+    }
+
+    private async Task BuscarDispositivos()
+    {
+        try
+        {
+            Device = await BluetoothNavigator.RequestDevice(
+            new RequestDeviceQuery
+            {
+                AcceptAllDevices = true
+            });
+        }
+        catch (Exception e)
+        {
+            Error = e.Message;
+        }
+    }
+
+    private async Task ConectarDispositivo(bool connect)
+    {
+        if (Device == null)
+            return;
+
+        Connected = connect;
+
+        if (connect)
+        {
+            await Device.Gatt.Connect();
+        }
+        else
+        {
+            await Device.Gatt.Disonnect();
+            Device = null;
+        }
+    }
 
 }
