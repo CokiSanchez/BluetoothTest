dc89a3777622c77e8613c607f644ac79d5c58b79
Jorge Sánchez Jorquera <jsanchez@smc.cl>
2024-02-21 16:37:19 -0300

    initchunk

--- BluetoothTest/Pages/Index.razor.cs
+++ BluetoothTest/Pages/Index.razor.cs
@@ -193,14 +193,16 @@ public partial class Index : IDisposable
             //var pixels = GetPixelValues(bytes, ancho);
 
             //var init = new byte[] { 0x1B, 0x2A, 0x21, (byte)(ancho % 256), (byte)Math.Floor((decimal)ancho / 256) };
-            var init = new byte[] { 0x1B, 0x2A, 0x21, (byte)(2 % 256), (byte)Math.Floor((decimal)2 / 256) };
+            //var init = new byte[] { 0x1B, 0x2A, 0x21, (byte)(2 % 256), (byte)Math.Floor((decimal)2 / 256) };
 
-            await Characteristic.WriteValueWithoutResponse(init);
+            //await Characteristic.WriteValueWithoutResponse(init);
 
             var b = new byte[] { 0xFF, 0x00, 0xFF, 0x00, 0xFF, 0x00 };
 
             foreach (var chunk in b.Chunk(3))
             {
+                var init = new byte[] { 0x1B, 0x2A, 0x21, (byte)(2 % 256), (byte)Math.Floor((decimal)2 / 256) };
+                await Characteristic.WriteValueWithoutResponse(init);
                 await Characteristic.WriteValueWithoutResponse(chunk);
             }
 
